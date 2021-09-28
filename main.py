8c61d88f7f77fe2880dfca0619947e959420590c
Jardi-land <Lorenzodezen@outlook.com>
2021-09-29 00:21:10 +0200

    Renamed all to _surf

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ screen = pygame.display.set_mode((screen_res[0], screen_res[1]))
 pygame.display.set_caption(window_name)
 clock = pygame.time.Clock()
 
-bg_surface = pygame.Surface((1920,1080))
-bg_surface.fill((118, 120, 134, 255))
+bg_surf = pygame.Surface((1920,1080))
+bg_surf.fill((118, 120, 134, 255))
 
-platform = pygame.image.load("game_files/platforms/Platform.png").convert_alpha()
-platform = pygame.transform.scale(platform, (165, 155))
-platform_thin = pygame.image.load("game_files/platforms/Platform_Thin.png").convert_alpha()
-platform_thin = pygame.transform.scale(platform_thin, (85, 155))
+platform_surf = pygame.image.load("game_files/platforms/Platform.png").convert_alpha()
+platform_surf = pygame.transform.scale(platform_surf, (165, 155))
+platform_thin_surf = pygame.image.load("game_files/platforms/Platform_Thin.png").convert_alpha()
+platform_thin_surf = pygame.transform.scale(platform_thin_surf, (85, 155))
 
 platform_x = 1755
 platform_y = 925
@@ -29,8 +29,8 @@ while True:
             pygame.quit()
             exit()
 
-    screen.blit(bg_surface,(0,0))
-    update_map_menu(platform_y, platform_x, platform, platform_thin)
+    screen.blit(bg_surf,(0,0))
+    update_map_menu(platform_y, platform_x, platform_surf, platform_thin_surf)
 
     pygame.display.update()
     clock.tick(60)
--- map_menu.py
+++ map_menu.py
@@ -28,7 +28,7 @@ firstmap = [["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
 
 screen = pygame.display.set_mode((screen_res[0], screen_res[1]))
 
-def update_map_menu(platform_y, platform_x, platform, platform_thin):
+def update_map_menu(platform_y, platform_x, platform_surf, platform_thin_surf):
     for i in range(18):
         if i == 0:
             platform_y = 925
@@ -41,10 +41,10 @@ def update_map_menu(platform_y, platform_x, platform, platform_thin):
             else:
                 platform_x = platform_x-160
             if firstmap[line_i][11-i] == "X":
-                screen.blit(platform,(platform_x,platform_y))
+                screen.blit(platform_surf,(platform_x,platform_y))
             elif firstmap[line_i][11-i] == " |":
                 platform_x = platform_x+80
-                screen.blit(platform_thin,(platform_x,platform_y))
+                screen.blit(platform_thin_surf,(platform_x,platform_y))
                 platform_x = platform_x-80
             elif firstmap[line_i][11-i] == "| ":
-                screen.blit(platform_thin,(platform_x,platform_y))
+                screen.blit(platform_thin_surf,(platform_x,platform_y))
