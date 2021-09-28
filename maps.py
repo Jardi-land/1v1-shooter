d58ef5356329dec467f630601fe5049d042f9831
Jardi-land <Lorenzodezen@outlook.com>
2021-09-28 21:56:36 +0200

    Added map generator

--- maps.py
+++ maps.py
@@ -1,7 +1,40 @@
+import sys, pygame
+
+screen_res = [1920, 1080]
+screen_scale = [1920/screen_res[0], 1080/screen_res[1]]
+
 firstmap = [["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
             ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
             ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
             ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
             ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
             ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+            ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+            ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+            ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+            ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+            ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+            ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+            ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+            ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+            ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+            ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+            ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
             ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],]
+
+screen = pygame.display.set_mode((screen_res[0], screen_res[1]))
+
+def update_map(platform_y, platform_x, platform):
+    for i in range(18):
+        if i == 0:
+            platform_y = 925
+        else:
+            platform_y = platform_y-60
+        line_i = 17-i
+        for i in range(12):
+            if i == 0:
+                platform_x = 1760
+            else:
+                platform_x = platform_x-160
+            if firstmap[line_i][11-i] == "X":
+                screen.blit(platform,(platform_x,platform_y))
