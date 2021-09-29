a60fa0f44c461aa89388977fe188c0498b16d980
Jardi-land <Lorenzodezen@outlook.com>
2021-09-29 19:44:44 +0200

    Changed Typo, added update

--- map_menu.py
+++ map_menu.py
@@ -6,15 +6,13 @@ import sys, pygame
 from settings import *
 from tiles import Tiles
 
-screen = pygame.display.set_mode((screen_res[0], screen_res[1]))
-
 class Level:
     def __init__(self,level_data,surface):
         self.display_surface = surface
         self.setup_level(level_data)
 
     def setup_level(self,layout):
-        self.tiles = pygame.sprite.Group()
+        self.Tiles = pygame.sprite.Group()
         for i in range(18):
                 if i == 0:
                     platform_y = 925
@@ -28,14 +26,15 @@ class Level:
                         platform_x = platform_x-160
                     if firstmap[line_i][11-i] == "X":
                         Tile = Tiles((platform_x,platform_y),1)
-                        self.tiles.add(Tile)
+                        self.Tiles.add(Tile)
                     elif firstmap[line_i][11-i] == " |":
                         platform_x = platform_x+80
                         Tile = Tiles((platform_x,platform_y),0)
-                        self.tiles.add(Tile)
+                        self.Tiles.add(Tile)
                         platform_x = platform_x-80
                     elif firstmap[line_i][11-i] == "| ":
                         Tile = Tiles((platform_x,platform_y),0)
-                        self.tiles.add(Tile)
+                        self.Tiles.add(Tile)
     def draw(self):
-        self.tiles.draw(self.display_surface)
+        self.Tiles.update(0)
+        self.Tiles.draw(self.display_surface)
--- tiles.py
+++ tiles.py
@@ -8,3 +8,5 @@ class Tiles(pygame.sprite.Sprite):
         else:
             self.image = pygame.transform.scale(pygame.image.load("game_files/platforms/Platform_Thin.png").convert_alpha(), (85, 155))
         self.rect = self.image.get_rect(topleft = pos)
+    def update(self,x_shift):
+        self.rect.x += x_shift
