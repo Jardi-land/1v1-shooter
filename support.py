fff3ce4fbf4946ed57962f8d2559cde5e5343294
Jardi-land <Lorenzodezen@outlook.com>
2021-09-29 22:26:00 +0200

    Changed frame scale (by 5)

--- support.py
+++ support.py
@@ -7,7 +7,7 @@ def import_folder(path):
     for _,__,img_files in walk(path):
         for image in img_files:
             full_path = path + "/" + image
-            image_surf = pygame.image.load(full_path).convert_alpha()
+            image_surf = pygame.transform.scale(pygame.image.load(full_path).convert_alpha(), (240, 240))
             surface_list.append(image_surf)
     
     return surface_list
