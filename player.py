9d3e389f787504435887ef41673cb14edf39a837
Jardi-land <Lorenzodezen@outlook.com>
2021-10-02 22:34:08 +0200

    Changed jump speed

--- player.py
+++ player.py
@@ -18,7 +18,7 @@ class Player(pygame.sprite.Sprite):
         self.direction = pygame.math.Vector2(0,0)
         self.speed = 8
         self.gravity = 0.8
-        self.jump_speed = -16
+        self.jump_speed = -20
 
         #Player Status
         self.status = "idle"
