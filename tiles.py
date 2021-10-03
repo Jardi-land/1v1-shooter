c935dd8ab7758cdef4e3a7e5dec06ca227d21dc5
Jardi-land <Lorenzodezen@outlook.com>
2021-10-03 15:57:16 +0200

    Changed tiles collisions

--- tiles.py
+++ tiles.py
@@ -5,10 +5,10 @@ class Tiles(pygame.sprite.Sprite):
         super().__init__()
         if type:
             self.image = pygame.transform.scale(pygame.image.load("game_files/platforms/Platform.png").convert_alpha(), (165, 155))
-            self.colliderect = pygame.Rect(pos[0], pos[1] + 55, 165, 100)
+            self.colliderect = pygame.Rect(pos[0], pos[1] + 55, 165, 60)
         else:
             self.image = pygame.transform.scale(pygame.image.load("game_files/platforms/Platform_Thin.png").convert_alpha(), (85, 155))
-            self.colliderect = pygame.Rect(pos[0], pos[1] + 55, 85, 100)
+            self.colliderect = pygame.Rect(pos[0], pos[1] + 55, 85, 60)
 
         self.rect = self.image.get_rect(topleft = pos)
         #self.colliderect = self.image.get_rect(topleft = (pos[0], pos[1] + 55))
