37ee5db3a27bedd2b72a947b25f502a89b34b355
Jardi-land <Lorenzodezen@outlook.com>
2021-10-10 02:06:33 +0200

    Changed bullet speed

--- bullet.py
+++ bullet.py
@@ -6,7 +6,7 @@ class Bullet(pygame.sprite.Sprite):
         super().__init__()
         if direct:
             self.image = pygame.transform.scale(pygame.image.load("game_files/gun_files/SpongeBullet.png").convert_alpha(), (int(15*screen_scale), int(5*screen_scale)))
-            self.speed = 40
+            self.speed = 50
         else:
             self.image = pygame.transform.flip(pygame.transform.scale(pygame.image.load("game_files/gun_files/SpongeBullet.png").convert_alpha(), (int(15*screen_scale), int(5*screen_scale))),True,False)
             self.speed = -40
