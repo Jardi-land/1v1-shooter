eee0fa9cfc29a27f4948c62e406043e0b279c9f7
Jardi-land <Lorenzodezen@outlook.com>
2021-10-12 22:46:48 +0200

    Changed type in tiles rect

--- tiles.py
+++ tiles.py
@@ -6,7 +6,7 @@ class Tiles(pygame.sprite.Sprite):
         super().__init__()
         if type:
             self.image = pygame.transform.scale(pygame.image.load("game_files/platforms/Platform.png").convert_alpha(), (int(165*screen_scale), int(155*screen_scale)))
-            self.colliderect = pygame.Rect(pos[0], pos[1] + 55*screen_scale, 85*screen_scale, 100*screen_scale)
+            self.colliderect = pygame.Rect(pos[0], pos[1] + 55*screen_scale, 165*screen_scale, 100*screen_scale)
         else:
             self.image = pygame.transform.scale(pygame.image.load("game_files/platforms/Platform_Thin.png").convert_alpha(), (int(85*screen_scale), int(155*screen_scale)))
             self.colliderect = pygame.Rect(pos[0], pos[1] + 55*screen_scale, 85*screen_scale, 100*screen_scale)
