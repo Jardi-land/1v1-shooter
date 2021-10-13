732f521d00020eed5a6a63e8dfe533e0d05f87d4
Redsteeler03 <NicolaA12@hotmail.com>
2021-10-13 11:11:15 +0200

    Update menu.py

--- menu.py
+++ menu.py
@@ -12,6 +12,7 @@ def main_menu():
     while True:
         screen.fill((0,0,0))
 
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
