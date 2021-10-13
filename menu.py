64bbddbe094bebeca4b197492eecca8e06846ca2
Redsteeler03 <NicolaA12@hotmail.com>
2021-10-13 11:09:58 +0200

    test menu 1.0

---
+++ menu.py
@@ -0,0 +1,24 @@
+import os
+import sys, pygame
+from settings import *
+from main import main
+
+def main_menu():
+    screen = pygame.display.set_mode((400, 400))
+    pygame.display.set_caption("Main menu")
+    clock = pygame.time.Clock()
+    pygame.mouse.set_visible(True)
+
+    while True:
+        screen.fill((0,0,0))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    exit()
+
