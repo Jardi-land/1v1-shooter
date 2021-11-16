0955137c5841fcf1b70ae7934ec77a709ada6b2c
Redsteeler03 <NicolaA12@hotmail.com>
2021-11-16 23:04:08 +0100

    added config file

--- config.py
+++ config.py
@@ -1,3 +1,21 @@
-MAX_HEALTH = 4
-WEAPON_COOLDOWN = 90
-GRAVITY_MODE = 'normal'
+import configparser as cp
+
+class Config:
+    def __init__(self, config_file) -> None:
+        self.MAX_HEALTH: int
+        self.WEAPON_COOLDOWN: int
+        self.GRAVITY_MODE: str
+        
+        
+        self.file = config_file
+        self.load(self.file)
+
+    def load(self, file):
+        Cfg = cp.ConfigParser()
+        Cfg.read(file)
+        self.MAX_HEALTH = Cfg.get('GameVars', 'MAX_HEALTH')
+        self.WEAPON_COOLDOWN = Cfg.get('GameVars', 'WEAPON_COOLDOWN')
+        self.GRAVITY_MODE = Cfg.get('GameVars', 'GRAVITY_MODE')
+
+
+cfg = Config('config.ini')
